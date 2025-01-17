8386197f2ed1a04278c170a92918991da9dda6e2
hz-uiw <kwonmc5.11@gmail.com>
2025-01-17 17:19:21 +0900

    axios, 백엔드 통신

---
+++ src/study/App14.jsx
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import React from 'react';
+
+// npm install axios
+function App14(props) {
+
+    const handleRequestOnClick = async () => {
+        let response = null;
+        try {
+            response = await axios.get("http://localhost:8080/servlet_study_war/api/user");
+            console.log(response);
+            console.log(response.data.username);
+        } catch(error) {
+            console.error(error);
+        }
+    }
+    return (
+        <div>
+            <button onClick={handleRequestOnClick}>요청</button>
+        </div>
+    );
+}
+
+export default App14;
