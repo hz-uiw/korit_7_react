d73138f59e96608e1187ec779ffaa12643da353c
hz-uiw <kwonmc5.11@gmail.com>
2025-01-20 12:49:46 +0900

    @Emotion/react

---
+++ src/pages/IndexPage/IndexPage.jsx
@@ -0,0 +1,14 @@
+/**@jsxImportSource @emotion/react */
+import { mainLayout } from './style';
+import { css } from '@emotion/react';
+import React from 'react';
+
+function IndexPage(props) {
+    return (
+        <div css={mainLayout("#fafafa")}>
+            
+        </div>
+    );
+}
+
+export default IndexPage;
---
+++ src/pages/IndexPage/style.js
@@ -0,0 +1,8 @@
+import { css } from "@emotion/react";
+
+export const mainLayout = (backgroundColor) => css`
+    width: 1100px;
+    height: 100%;
+    margin: 0 auto;
+    background-color: ${backgroundColor};
+`;
