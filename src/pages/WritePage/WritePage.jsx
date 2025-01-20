8b8e4ada295fae9034854603915c17497ea980b0
hz-uiw <kwonmc5.11@gmail.com>
2025-01-20 16:38:11 +0900

    quill

--- src/pages/WritePage/WritePage.jsx
+++ src/pages/WritePage/WritePage.jsx
@@ -1,11 +1,21 @@
 /**@jsxImportSource @emotion/react */
+import ReactQuill from 'react-quill';
 import * as s from './style';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function WritePage(props) {
+
+    useEffect(() => {
+        const head = document.querySelector("head");
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = "https://unpkg.com/react-quill@1.3.3/dist/quill.snow.css"
+        head.appendChild(link);
+    }, []);
+
     return (
         <div>
-            
+            <ReactQuill />
         </div>
     );
 }
