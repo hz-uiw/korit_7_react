013a93a240b861727d38f615a55e0c51b61e4a34
hz-uiw <kwonmc5.11@gmail.com>
2025-01-20 12:16:20 +0900

    router

---
+++ src/components/RouterDom/Route.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+
+function Route({pathname, path, element}) {
+
+  return (
+    <>
+      {
+        window.location.pathname === path && element
+      }
+    </>
+  );
+}
+
+export default Route;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import ReactDOM from 'react-dom/client';
-import App from './study/App';
-import App2 from './study/App2';
-import App3 from './study/App3';
-import App4 from './study/App4';
-import App5 from './study/App5';
-import App6 from './study/App6';
-import App7 from './study/App7';
-import App8 from './study/App8';
-import App9 from './study/App9';
-import App10 from './study/App10';
-import App11 from './study/App11';
-import App12 from './study/App12';
-import App13 from './study/App13';
-import App14 from './study/App14';
-// import App from './App';
+import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import RouterDom from './components/RouterDom/RouterDom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 // root.render(<App />);
 // root.render(<App2 />);
@@ -30,4 +24,4 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(<App11 />);
 // root.render(<App12 />);
 // root.render(<App13 />);
-root.render(<App14 />);
+// root.render(<App14 />);
