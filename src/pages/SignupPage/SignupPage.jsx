39ce177d0e6eeede30eb895afb47eb1a7123ef24
hz-uiw <kwonmc5.11@gmail.com>
2025-01-22 11:03:10 +0900

    회원가입

---
+++ src/pages/SignupPage/SignupPage.jsx
@@ -0,0 +1,94 @@
+/**@jsxImportSource @emotion/react */
+import { use } from 'react';
+import * as s from './style';
+import React, { useRef } from 'react';
+
+function SignupPage(props) {
+    // useStatus에서 set함수가 필요없으면 없어도 useStatus 사용 가능능
+    // 요소의 개체를 직접 접근하기 위해서 useRef() 사용
+    const [inputRefs] = useState([useRef(), useRef(), useRef(), useRef()]);
+    const [buttonRefs] = useState([useRef() ]);
+    const [inputValue, setInputValue] = usestate({
+        username: "",
+        password: "",
+        name: "",
+        email: "",
+    });
+
+    const handleInputOnChange = () => {
+        setInputValue({
+            ...inputValue,
+            [e.target.name]: e.target.value,
+        });
+
+    const handleInputOnKeyDown = (e) => {
+        if(e.keyCode === 13) {
+            let foundIndex = -1;
+            for(let i = 0; i < inputRefs.length; i++) {
+                if(inputRefs[i].current === e.target) {
+                    foundIndex = i;
+                    break;
+                }
+            }
+            // 마지막 인덱스에서 엔터를 눌리면 가입버튼 클릭되는 함수수
+            // inputRefs. length - 1 마지막 인덱스
+            if(foundIndex === inputRefs. length - 1) {
+                buttonRefs[0].current.click();
+                return;
+            }
+            inputRefs[foundIndex + 1].current.focus();
+        }
+    }
+
+    const handleSignupSubmitOnClick = () => {
+        try {
+            const response = axios.post("http://localhost:8080/servlet_study_war/api/signup", inputValue);
+        } catch(error) {
+
+        }
+    }
+
+
+    }
+    return (
+        <div>
+            <div>
+                <input type="text" 
+                    placeholder='사용자 이름' 
+                    name='username' 
+                    value={inputValue.username} 
+                    onChange={handleInputOnChange} 
+                    onKeyDown={handleInputOnKeyDown} 
+                    ref={inputRefs[0]}/>
+                <input type="password" 
+                    placeholder='비밀번호' 
+                    name='password' 
+                    value={inputValue.password} 
+                    onChange={handleInputOnChange} 
+                    onKeyDown={handleInputOnKeyDown} 
+                    ref={inputRefs[1]}/>
+                <input type="text" 
+                    placeholder='성명' 
+                    name='name' 
+                    value={inputValue.name} 
+                    onChange={handleInputOnChange} 
+                    onKeyDown={handleInputOnKeyDown} 
+                    ref={inputRefs[2]}/>
+                <input type="text" 
+                    placeholder='이메일 주소' 
+                    name='email' 
+                    value={inputValue.email} 
+                    onChange={handleInputOnChange} 
+                    onKeyDown={handleInputOnKeyDown} 
+                    ref={inputRefs[3]}/>
+                <button ref={buttonRefs[0]}>가입</button>
+            </div>
+            <div>
+                <span>계정이 있으신가요? </span>
+                <Link to={"/signin"}>로그인</Link>
+            </div>
+        </div>
+    );
+}
+
+export default SignupPage;
